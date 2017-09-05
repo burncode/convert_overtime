a5e531bf53ecccff630ac4d3693dfaa223405412
ffxgamer <ffxgamer@163.com>
2017-09-05 11:39:47 +0800

    ver1.1.0

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # convert_overtime
 使用python的excel处理模块openpyxl制作
 
+
+## ver1.1.0 20170905
+* 增加判断是否为工作日,如果是工作日的话,时间从17:30开始
+
 ## ver1.0.0 20170904
 * 输入的excel放咋爱src目录内, 输出模板放在dst目录内
 * 只能接受.xlsx的excel
--- convert_overtime.py
+++ convert_overtime.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Description: 转换两张表用的
+# Version: 1.1.0
+# Date: 20170905
 
 
 import openpyxl
 import os
+import datetime
 
 
 # 函数用于打开表并保存激活表
@@ -11,6 +15,32 @@ def openxl(xlsx):
     wb = openpyxl.load_workbook(xlsx)
     sheet = wb.get_active_sheet()
     return sheet
+
+# 转换time的起始时间为17:30
+def time_to_1730(time):
+    x, y  = time.split('-')
+    result = "17:30" + '-' + y
+    return result
+
+# 验证日期是否是工作日
+def is_weekday(date):
+    # 误差需-2
+    date = int(date) - 2
+    delta = datetime.timedelta(days=date)
+    date_1900 = datetime.datetime(1900, 1, 1)
+    date = date_1900 + delta
+    weekday = date.isoweekday()
+    for i in range(1, 6):
+        if i == weekday:
+            return True
+    return False
+
+# 如果是date是工作日, 转换time为17:30开始到结束
+def weekday_time(time, date):
+    if is_weekday(date):
+        return time_to_1730(time)
+    else:
+        return time
 
 # 读取源表的值,写入目标表中相应位置
 def convert(src_sheet):
@@ -28,6 +58,7 @@ def convert(src_sheet):
                 time = src_sheet.cell(column=i-1, row=j).value
                 if time == None:
                     continue
+                time = weekday_time(time, date)
                 # 打印时长
                 overtime = src_sheet.cell(column=i, row=j).value
                 if overtime == None:
@@ -37,7 +68,6 @@ def convert(src_sheet):
                 src_data.setdefault(date, {})
                 src_data[date].setdefault(name, [time, overtime])
     return src_data
-
 
 # 写入目标xlsx
 def writexlsx(src_data, dst_sheet):
@@ -69,7 +99,6 @@ def main():
     wb = openpyxl.load_workbook(dst_xlsx)
     sheet = wb.get_active_sheet()
     writexlsx(src_date, sheet)
-    print src_xlsx, dst_xlsx
     wb.save(dst_xlsx)
 
 if __name__ == '__main__':
